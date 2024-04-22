30e5716d31b45b7d64e67d1510a02b79fd10c226
Mariya Zaharieva <mariqtrunkova@gmail.com>
2024-04-22 23:25:57 +0300

    Init project

---
+++ my-task-b/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import NavBar from "./Components/Header/NavBar";
+import '@fortawesome/fontawesome-free/css/all.css';
+
+export default function Home() {
+  return (
+    <NavBar/>
+  );
+}
