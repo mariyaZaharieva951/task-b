2f27943267441b8a29f763b5ec7762c3b111f814
Mariya Zaharieva <mariqtrunkova@gmail.com>
2024-04-26 11:25:28 +0300

    Update Footer component

--- my-task-b/src/app/Components/Footer/Mail.tsx
+++ my-task-b/src/app/Components/Footer/Mail.tsx
@@ -2,13 +2,13 @@ const Mail = () => {
 
     return (
         <div className="flex justify-center gap-12 content-center font-custom-font bg-gray-dark py-10">
-            <h3 className="max-w-xs text-sm font-bold text-white text-wrap pr-3">
+            <h3 className="max-w-sm text-base font-bold text-white text-wrap mr-10 pr-10">
                 Отстъпки и специални предложения налични само на имейл
             </h3>
-            <form className="flex flex-col gap-2">
+            <form className="flex flex-col gap-2 px-10 mr-10">
                 <div className="flex text-[10px]">
-                    <input className="text-gray-tx p-0.5 pr-20" placeholder="Въведете вашия имейл"></input>
-                    <button className="bg-red-bg text-white pr-5 end">Абонирай се</button>
+                    <input className="text-gray-tx p-2 pr-20" placeholder="Въведете вашия имейл"></input>
+                    <button className="bg-red-bg text-white pl-2 pr-7 end">Абонирай се</button>
                 </div>
                 <div>
                     <p className="text-[10px] text-slate-100">Всеки месец ще получаваш първи най-добрите ни оферти</p>
--- my-task-b/src/app/Components/Partners/Partners.tsx
+++ my-task-b/src/app/Components/Partners/Partners.tsx
@@ -1,39 +1,39 @@
 const Partners = () => {
 
     return (
-        <div className="max-w-[40%] m-auto font-custom-font my-12 py-8">
+        <div className="max-w-[60%] m-auto font-custom-font my-10 py-5">
       <h2 className="text-lg font-bold mb-2">Партньори</h2>
       <p className="text-xs w-[55%] mb-8">Our customers accomplish amazing things every day. They work to find cures to cancer, travel to space.</p>
-      <div className="grid grid-cols-5 gap-3 text-center">
-        <div className="col-span-1">
-          <img src="./assets/p1.png" alt="Image 1" className="w-30 h-5" />
+      <div className="grid grid-cols-5 gap-7 text-center">
+        <div className="col-span-1 mr-5">
+          <img src="./assets/p1.png" alt="Image 1" className="w-40 h-7" />
         </div>
-        <div className="col-span-1">
-          <img src="./assets/p2.png" alt="Image 1" className="w-30 h-5" />
+        <div className="col-span-1 mr-5">
+          <img src="./assets/p2.png" alt="Image 1" className="w-40 h-7" />
         </div>
-        <div className="col-span-1">
-          <img src="./assets/p3.png" alt="Image 1" className="w-30 h-5" />
+        <div className="col-span-1 mr-5">
+          <img src="./assets/p3.png" alt="Image 1" className="w-40 h-7" />
         </div>
-        <div className="col-span-1">
-          <img src="./assets/p4.png" alt="Image 1" className="w-30 h-5" />
+        <div className="col-span-1 mr-5">
+          <img src="./assets/p4.png" alt="Image 1" className="w-40 h-7" />
         </div>
-        <div className="col-span-1">
-          <img src="./assets/p5.png" alt="Image 1" className="w-30 h-5" />
+        <div className="col-span-1 mr-5">
+          <img src="./assets/p5.png" alt="Image 1" className="w-40 h-7" />
         </div>
-        <div className="col-span-1">
-          <img src="./assets/p6.png" alt="Image 1" className="w-30 h-5 " />
+        <div className="col-span-1 mr-5">
+          <img src="./assets/p6.png" alt="Image 1" className="w-40 h-7 " />
         </div>
-        <div className="col-span-1">
-          <img src="./assets/p7.png" alt="Image 1" className="w-30 h-5" />
+        <div className="col-span-1 mr-5">
+          <img src="./assets/p7.png" alt="Image 1" className="w-40 h-7" />
         </div>
-        <div className="col-span-1">
-          <img src="./assets/p8.png" alt="Image 1" className="w-30 h-5" />
+        <div className="col-span-1 mr-5">
+          <img src="./assets/p8.png" alt="Image 1" className="w-40 h-7" />
         </div>
-        <div className="col-span-1">
-          <img src="./assets/p9.png" alt="Image 1" className="w-30 h-5" />
+        <div className="col-span-1 mr-5">
+          <img src="./assets/p9.png" alt="Image 1" className="w-40 h-7" />
         </div>
-        <div className="col-span-1">
-          <img src="./assets/p10.png" alt="Image 1" className="w-30 h-5" />
+        <div className="col-span-1 mr-5">
+          <img src="./assets/p10.png" alt="Image 1" className="w-40 h-7" />
         </div>
         
       </div>
