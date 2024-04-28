d33212c43f83e60d30ec8950709c792a38f3f1ed
Mariya Zaharieva <mariqtrunkova@gmail.com>
2024-04-28 14:54:00 +0300

    Add scroller

--- my-task-b/src/app/Components/Program/Program.tsx
+++ my-task-b/src/app/Components/Program/Program.tsx
@@ -4,12 +4,12 @@ import Days from "./Days";
 const Program = () => {
 
     return (
-        <div>
+        <div id="right-content">
         <div className="bg-gray-bg mt-[-120px]">
             <div className="">
             </div>
         </div>
-        <div className="bg-gray-bg mt-10 pt-10 ml-10 font-custom-font">
+        <div className="bg-gray-bg mt-10 pt-10 pb-10 font-custom-font">
             <h2 className="mt-10 pt-10 ml-10 mb-5 pl-10 text-xl font-bold">Програма</h2>
             <div className="ml-10 pl-10"> <Days/>
             </div>
--- my-task-b/src/app/page.tsx
+++ my-task-b/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+'use client';
 import NavBar from "./Components/Header/NavBar";
 import '@fortawesome/fontawesome-free/css/all.css';
 import Hero from "./Components/Hero/Hero";
@@ -13,22 +13,39 @@ import Partners from "./Components/Partners/Partners";
 import Footer from "./Components/Footer/Footer";
 import Clients from "./Components/About/Clients";
 import Covers from "./Components/Covers/Covers";
+import { useEffect, useState } from 'react';
+
 
 export default function Home() {
+
+  const [rightHeight, setRightHeight] = useState(0);
+
+  useEffect(() => {
+    const rightElement = document.getElementById('right-content');
+    if (rightElement) {
+      const height = rightElement.clientHeight;
+      
+      setRightHeight(height);
+    }
+  }, []);
+
+
   return (
-    <main>
+    <main className="overflow-y-auto">
       <NavBar/>
       <Hero/>
-      <div className="flex justify-content: flex-end">
-      <div className="w-1/2 ml-10 pl-5">
+      <div className="flex justify-content: flex-end ml-10 pl-10">
+      <div className="w-1/2 ml-12 pl-12 pr-5 overflow-y-auto my-2">
+        <div className=""  style={{ height: `${rightHeight}px`, scrollbarWidth: 'none' }}>
         <About />
         <Gallery/>
         <Promo/>
         <Singers/>
         <Hotels/>
         <Clients/>
+        </div>
       </div>
-      <div className="w-1/2 ml-10 pl-5">
+      <div className="w-1/2">
         <Program />
       </div>
       </div>
